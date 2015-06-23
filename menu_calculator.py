4fecb1dfe3b1e4eaddbf65422e90439ffe6cc2d6
beatewild <beatewild@users.noreply.github.com>
2015-06-23 15:15:34 -0700

    Create menu_calculator.py
    new file

---
+++ menu_calculator.py
@@ -0,0 +1,36 @@
+"""
+
+French Fries - $3.50 Hamburger - $5.50 Hotdog - $4.50 Burrito - $6.25 Chicken Salad - $9.50
+
+Soda - $1.75 Coffee - $2.50 Milk Shake - $4.25 Beer - $4.50 Wine - $6.50
+
+The tipp options are: 15%, 20%, 25%
+
+"""
+
+#Making a list of the items plus price
+#What and how much had the customer ordered?
+#Create a function to calculate the sum
+#Calculate tax
+#Calculate tip
+#Add tax and tip
+
+food =["French Fries" = 3.50, "Hamburger" = 5.50, "Hotdog" = 4.50, "Chicken Salad" = 9.50]
+drinks = ["Soda" = 1.75, "Coffee" = 2.50, "Milk Shake" = 4.25, "Beer" = 4.50, "Wine" = 6.50]
+tax = 8.75%
+
+
+Charly: 1 French Fries, 1 Hamburger, 2 Beer
+
+name = raw_input ("Name of the customer:")
+food = raw_input ("Which food did you eat?")
+drinks = raw_input ("What did you drink?")
+tip = raw_input("Enter the tip amount:")
+
+name_total = ((food + drinks) * tax) + tip 
+
+print name "has a total of", name_total
+
+
+
+
