3fcf01df85e508362e83bd143a71ed2204ea0e95
beatewild <beatewild@users.noreply.github.com>
2015-06-24 15:20:06 -0700

    Update menu_calculator.py
    "first calculation"

--- menu_calculator.py
+++ menu_calculator.py
@@ -15,21 +15,32 @@ The tipp options are: 15%, 20%, 25%
 #Calculate tip
 #Add tax and tip
 
-food =["French Fries" = 3.50, "Hamburger" = 5.50, "Hotdog" = 4.50, "Chicken Salad" = 9.50]
-drinks = ["Soda" = 1.75, "Coffee" = 2.50, "Milk Shake" = 4.25, "Beer" = 4.50, "Wine" = 6.50]
-tax = 8.75%
+
+food = {"French Fries":3.50, "Hamburger":5.50, "Hotdog":4.50, "Chicken Salad":9.50}
+drinks = {"Soda":1.75, "Coffee":2.50, "Milk Shake":4.25, "Beer":4.50, "Wine":6.50}
+tip = {"15":1.15,"20":1.20,"25":1.25}
+
+#tax = 8.75% #1.0875
+#tip = 15%, 20%, 25%
+
+'''
+name = raw_input ("What is your name: ")
+food_choice = raw_input ("Which food did you eat? ")
+drink_choice = raw_input ("What did you drink? ")
+tip_choice = raw_input("15 or 20 or 25: Choose the tip in percent: ")
+'''
 
 
-Charly: 1 French Fries, 1 Hamburger, 2 Beer
 
-name = raw_input ("Name of the customer:")
-food = raw_input ("Which food did you eat?")
-drinks = raw_input ("What did you drink?")
-tip = raw_input("Enter the tip amount:")
+food_choice = raw_input("Which food did you eat? ") 
+drink_choice = raw_input ("What did you drink? ")
+tip_choice = raw_input("15 or 20 or 25: Choose tip in percent: ")
 
-name_total = ((food + drinks) * tax) + tip 
 
-print name "has a total of", name_total
+total = ((food[food_choice] + drinks[drink_choice]) * 1.0875) * tip[tip_choice]
+
+print "You have to pay", total, "Dollar - included", tip_choice, "%","tip."
+
 
 
 
