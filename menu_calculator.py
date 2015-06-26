ec36001a03cee2ed7c8fd6f5eee825472b781ffa
beatewild <beatewild@users.noreply.github.com>
2015-06-25 21:54:41 -0700

    Update menu_calculator.py
    total food plus total drinks

--- menu_calculator.py
+++ menu_calculator.py
@@ -1,9 +1,3 @@
-
-
-food = {"French Fries":3.50, "Hamburger":5.50, "Hotdog":4.50, "Chicken Salad":9.50, "Pizza":14.00}
-drinks = {"Soda":1.75, "Coffee":2.50, "Milk Shake":4.25, "Beer":4.50, "Wine":6.50}
-tip = {"15":1.15,"20":1.20,"25":1.25}
-
 #tax = 8.75% #1.0875
 #tip = 15%, 20%, 25%
 
@@ -17,6 +11,11 @@ total = ((food[food_choice] + drinks[drink_choice]) * 1.0875) * tip[tip_choice]
 
 print "You have to pay", total, "Dollar - included", tip_choice, "%","tip."
 '''
+
+food = {"French Fries":3.50, "Hamburger":5.50, "Hotdog":4.50, "Chicken Salad":9.50, "Pizza":14.00}
+drink = {"Soda":1.75, "Coffee":2.50, "Milk Shake":4.25, "Beer":4.50, "Wine":6.50}
+tip = {"15":1.15,"20":1.20,"25":1.25}
+
 
 food_choice_list = []
 food_number_list = []
@@ -37,19 +36,53 @@ for i in range(len(food_choice_list)):
 	print i,"is i"
 	print food_choice_list[i],"is food_choice_list[i]"
 
-answer=0
+total_food=0
 for e in range(len(food_number_list)):
 	print e, "is e"
 	print food_number_list[e], "is food_number_list[e]"	
 
 
-answer = [float(food_choice_list) * float(food_number_list) for food_choice_list,food_number_list in zip(food_choice_list,food_number_list)]
+total_food = [float(food_choice_list) * float(food_number_list) for food_choice_list,food_number_list in zip(food_choice_list,food_number_list)]
+
+print "The total of food is ", sum(total_food), "dollars."
 
 
-print "The answer is", answer
+drink_choice_list = []
+drink_number_list = []
+items = []
+while(True):
+	drink_choice = raw_input("What did you drink? ") 
+	drink_number = raw_input("How many? ")
+	item = raw_input("Did you have more drinks? ")
+	drink_choice_list.append(drink_choice)
+	drink_number_list.append(drink_number)
+	if (item == "no"):
+		break	
+	else:
+		items.append(item)
+
+for i in range(len(drink_choice_list)):
+	drink_choice_list[i]=drink[drink_choice_list[i]]
+	print i,"is i"
+	print drink_choice_list[i],"is drink_choice_list[i]"
+
+total_drink=0
+for e in range(len(drink_number_list)):
+	print e, "is e"
+	print drink_number_list[e], "is drink_number_list[e]"	
+
+
+total_drink = [float(drink_choice_list) * float(drink_number_list) for drink_choice_list,drink_number_list in zip(drink_choice_list,drink_number_list)]
+
+print "The total of drinks is ", sum(total_drink), "dollars."
+
+print "The total of your check is", sum(total_food+total_drink), "dollars."
+
 
 
 '''
+
+
 items = []
 while(True):
 	drink_choice = raw_input("What did you drink? ")
@@ -59,8 +92,8 @@ while(True):
 		break	
 	else:
 		items.append(item)
-'''
 
+'''
 
 
 
