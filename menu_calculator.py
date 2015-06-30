c6cb6d334ae2d408b5d5c83dd10aba0f8d23594f
beatewild <beatewild@users.noreply.github.com>
2015-06-29 20:10:45 -0700

    Update menu_calculator.py
    "correction lowercase input and replace spaces from the prompt answers

--- menu_calculator.py
+++ menu_calculator.py
@@ -9,8 +9,8 @@
 #print "You have to pay", total, "Dollar - included", tip_choice, "%","tip."
 
 
-food = {"French Fries":3.50, "Hamburger":5.50, "Hotdog":4.50, "Salad":9.50, "Pizza":14.00, "Tiramisu":6.25, "Ice Cream":4.75}
-drink = {"Soda":1.75, "Coffee":2.50, "Milk Shake":4.25, "Beer":4.50, "Wine":6.50, "Gin":5.75, "Tea":2.25}
+food = {"french fries":3.50, "hamburger":5.50, "hotdog":4.50, "burrito":6.75, "salad":9.50, "pizza":14.00, "tiramisu":6.25, "ice cream":4.75}
+drink = {"soda":1.75, "tea":2.25, "coffee":2.75, "milk shake":4.25, "beer":4.50, "wine":6.50, "gin":5.75}
 tip = {"15":1.15,"20":1.20,"25":1.25}
 
 
@@ -18,9 +18,12 @@ food_choice_list = []
 food_number_list = []
 items = []
 while(True):
-	food_choice = raw_input("Which food did you eat? ") 
-	food_number = raw_input("How many? ")
-	item = raw_input("Did you eat more food? ")
+	food_choice = raw_input("French Fries, Hamburger, Hotdog, Burrito, Salad, Pizza, Tiramisu, Ice Cream: Which food did you eat? ").lower() 
+	food_choice = food_choice.replace(" ","")
+	food_number = raw_input("How many? ").lower()
+	food_number = food_number.replace(" ","")
+	item = raw_input("Did you eat more food? yes or no: ").lower()
+	item = item.replace(" ","")
 	food_choice_list.append(food_choice)
 	food_number_list.append(food_number)
 	if (item == "no"):
@@ -41,9 +44,12 @@ drink_choice_list = []
 drink_number_list = []
 items = []
 while(True):
-	drink_choice = raw_input("What did you drink? ") 
-	drink_number = raw_input("How many? ")
-	item = raw_input("Did you have more drinks? ")
+	drink_choice = raw_input("Soda, Tea, Coffee, Milk Shake, Beer, Wine, Gin: What did you drink? ").lower()
+	drink_choice = drink_choice.replace(" ","")
+	drink_number = raw_input("How many? ").lower()
+	drink_number = drink_number.replace(" ","")
+	item = raw_input("Did you have more drinks? yes or no: ").lower()
+	item = item.replace(" ","")
 	drink_choice_list.append(drink_choice)
 	drink_number_list.append(drink_number)
 	if (item == "no"):
@@ -62,6 +68,7 @@ total_drink = [float(drink_choice_list) * float(drink_number_list) for drink_cho
 total_net = sum(total_food+total_drink)
 
 tip_choice = raw_input("15 or 20 or 25: Choose the tip in percent: ")
+tip_choice = tip_choice.replace(" ","")
 
 #print "The total of your check is", total_net, "dollars."
 
@@ -72,12 +79,6 @@ total_tax = total_net * 1.0875
 
 total_tax_tip = total_tax * tip[tip_choice]
 
-print "You have to pay", total_tax_tip, "Dollars - included 8.75","%","tax and", tip_choice, "%","tip."
-
-
-
-
-#Question: How to receive only two decimals after the coma? float? .2f?
-
+print "You have to pay", "%.2f" % total_tax_tip, "Dollars. 8.75","%","tax and", tip_choice, "%","tip included. Thanks for your visit."
 
 
